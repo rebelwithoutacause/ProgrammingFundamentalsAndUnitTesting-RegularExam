b8249d5af30e8f52ecb2a305cc94ace611b7f4a3
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-04-17 10:39:28 +0300

    Add files via upload

---
+++ 02. Average-and-Total-Rainfall-Problem/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] dailyRainfallAmount = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+int startDay = int.Parse(Console.ReadLine());
+int endDay = int.Parse(Console.ReadLine());
+
+int totalRainfall = 0;
+int count = 0;
+
+for(int i = startDay; i <= endDay; i++) 
+{
+ totalRainfall += dailyRainfallAmount[i];
+    count++;
+}
+double averageRainfall = (double)totalRainfall / count;
+
+Console.WriteLine(totalRainfall);
+Console.WriteLine($"{averageRainfall:F2}");
