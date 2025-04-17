19723cf13445f2c24e09b261cba643be06a3210f
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-04-17 10:44:05 +0300

    Add files via upload

---
+++ 04. Consonants-Counter-Problem/TestApp.Tests/ConsonantsCounterTests.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace TestApp.Tests
+{
+    [TestFixture]
+    public class ConsonantsCounterTests
+    {
+        [Test]
+        public void Test_CountTotalConsonants_GetEmptyList_ReturnsZero()
+        {
+            
+        }
+
+        [Test]
+        public void Test_CountTotalConsonants_GetListWithEmptyStringValues_ReturnsZero()
+        {
+           
+        }
+
+        [Test]
+        public void Test_CountTotalConsonants_MultipleStrings_ReturnsConsonantsCount()
+        {
+            
+        }
+
+        [Test]
+        public void Test_CountTotalConsonants_GetStringsWithNoConsonants_ReturnsZero()
+        {
+           
+        }
+
+        [Test]
+        public void Test_CountTotalConsonants_StringsWithMixedCaseConsonants_ReturnsConsonantsCount()
+        {
+            
+        }
+    }
+
+}
+
