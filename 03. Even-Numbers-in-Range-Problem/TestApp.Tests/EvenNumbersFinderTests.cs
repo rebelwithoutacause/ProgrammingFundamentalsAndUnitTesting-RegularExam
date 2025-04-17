7f863521690ab558a9d073eac6d873a29c646012
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-04-17 10:42:13 +0300

    Add files via upload

---
+++ 03. Even-Numbers-in-Range-Problem/TestApp.Tests/EvenNumbersFinderTests.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+
+namespace TestApp.Tests
+{
+    [TestFixture]
+    public class EvenNumbersFinderTests
+    {
+        [Test]
+        public void Test_FindEvenNumbers_StartGreaterThanEnd_ReturnsErrorMessage()
+        {
+           
+        }
+
+        [Test]
+        public void Test_FindEvenNumbers_NoEvenNumbersInRange_ReturnsEmptyString()
+        {
+
+        }
+
+        [Test]
+        public void Test_FindEvenNumbers_SingleEvenNumberInRange_ReturnsThatNumber()
+        {
+
+        }
+
+        [Test]
+        public void Test_FindEvenNumbers_MultipleEvenNumbersInRange_ReturnsEvenNumbers()
+        {
+
+        }
+
+        [Test]
+        public void Test_FindEvenNumbers_ZeroInRange_ReturnsZero()
+        {
+
+        }
+    }
+}
