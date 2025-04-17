f785b81f9c8f77194c1df02c71f4d23746f8460c
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-04-17 10:37:01 +0300

    Add files via upload

---
+++ 01. Water-Consumption-Problem/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01.ProblemOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberN = int.Parse(Console.ReadLine());
+
+            if (numberN <= 0) 
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            
+            int comulativeWaterConsumption = 0;
+
+            for(int i = 1; i <= numberN; i++) 
+            {
+                int dailyConsumption = int.Parse(Console.ReadLine());
+                comulativeWaterConsumption += dailyConsumption;
+                Console.WriteLine(comulativeWaterConsumption);
+            }
+
+        }
+    }
+}
